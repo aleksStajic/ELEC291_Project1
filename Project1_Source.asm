228db74d4be0c90152443e13cf617dcd7f7b6942
aleksStajic <sstajic10@gmail.com>
2022-02-10 15:24:53 -0800

    Added capacitor detection with timer2 and timer1 and 555 timers

--- Project1_Source.asm
+++ Project1_Source.asm
@@ -8,11 +8,12 @@ org 0000H
 ; Timer/Counter 0 overflow interrupt vector
 org 0x000B
 	ljmp Timer0_ISR
-   
+; Timer/Counter 1 overflow interrupt vector
+org 0x001B
+	reti   
 ; Timer/Counter 2 overflow interrupt vector
 org 0x002B
     reti
-	
 
 ; These register definitions needed by 'math32.inc'
 DSEG at 30H
@@ -57,6 +58,7 @@ $LIST
 ;                     1234567890123456    <- This helps determine the location of the counter
 Initial_Message:  db 'Period (ns):   ', 0
 No_Signal_Str:    db 'No signal      ', 0
+No_Signal_Str1:  db 'No signal T1', 0
 
 ; Sends 10-digit BCD number in bcd to the LCD
 Display_10_digit_BCD:
@@ -103,7 +105,18 @@ InitTimer2:
 	setb ET2
     ret
     
-
+InitTimer1:
+	mov a, TMOD
+	anl a, #0x0f ; Clear the bits for timer 1
+	orl a, #0x10 ; Configure timer 1 as 16-timer
+	mov TMOD, a
+	mov TH1, #0
+	mov TL1, #0
+	; Set autoreload value
+	mov RH1, #0
+	mov RL1, #0
+	clr IE1 ; set timer1 interrupt to 0
+	ret
     
 Init_Seed:
 	; Wait for a push of the BOOT button
@@ -148,6 +161,7 @@ Wait_Random:
 Initialize_All:
     lcall InitTimer2
     lcall Timer0_Init
+    lcall InitTImer1
     lcall LCD_4BIT ; Initialize LCD
     setb EA
     lcall Init_Seed
@@ -161,12 +175,13 @@ main:
     ; Initialize the hardware:
     mov SP, #7FH
     lcall Initialize_All
-    setb P0.0 ; Pin is used as input for 555 timer
+    setb P0.0 ; Pin is used as input for 555 timer for timer/counter2
+    setb P0.1 ; Pin for 555 timer for timer/counter1
     clr HLbit 
     clr TR0 ; clear timer 0 so no sound when game first starts
 
 	Set_Cursor(1, 1)
-    Send_Constant_String(#Initial_Message) 
+    ;Send_Constant_String(#Initial_Message) 
     
 
 forever:
@@ -205,7 +220,7 @@ pin0period:
     clr TR2 ; Stop timer 2
     mov TL2, #0
     mov TH2, #0
-    clr TF2
+    clr TF2 ; clear timer2 overflow flag
     setb TR2
 synch1:
 	jb TF2, no_signal ; If the timer overflows, we assume there is no signal
@@ -236,9 +251,60 @@ measure2:
     Load_y(1000)
     lcall div32
     
-    Load_y(200000)
+    Load_y(208000)
     lcall x_lt_y
     jb mf, no_signal
+    
+
+	; Convert the result to BCD and display on LCD
+	Set_Cursor(1, 1)
+	lcall hex2bcd
+	lcall Display_10_digit_BCD
+    ret 
+    
+no_signal:	
+	Set_Cursor(1, 1)
+    Send_Constant_String(#No_Signal_Str)
+        
+pin1period:
+    ; synchronize with rising edge of the signal applied to pin P0.0
+    clr TR1 ; Stop timer 2
+    mov TL1, #0
+    mov TH1, #0
+    clr TF1 ; clear timer1 overflow flag
+    setb TR1
+synch1_1:
+	jb TF1, no_signal_1 ; If the timer overflows, we assume there is no signal
+    jb P0.1, synch1_1
+synch2_1:    
+	jb TF1, no_signal_1
+    jnb P0.1, synch2_1
+    
+    ; Measure the period of the signal applied to pin P0.0
+    clr TR1
+    mov TL1, #0
+    mov TH1, #0
+    clr TF1
+    setb TR1 ; Start timer 2
+measure1_1:
+	jb TF1, no_signal_1
+    jb P0.1, measure1_1
+measure2_1:    
+	jb TF1, no_signal_1
+    jnb P0.1, measure2_1
+    clr TR1 ; Stop timer 2, [TH2,TL2] * 45.21123ns is the period
+    Load_y(45211)
+    mov x+0, TL1
+    mov x+1, TH1
+    mov x+2, #0
+    mov x+3, #0
+    lcall mul32
+    Load_y(1000)
+    lcall div32
+    
+    Load_y(210000)
+    lcall x_lt_y
+    jb mf, no_signal_1
     
 
 	; Convert the result to BCD and display on LCD
@@ -247,8 +313,8 @@ measure2:
 	lcall Display_10_digit_BCD
     ret 
     
-no_signal:	
+no_signal_1:	
 	Set_Cursor(2, 1)
-    Send_Constant_String(#No_Signal_Str)
+    Send_Constant_String(#No_Signal_Str1)
     ljmp pin0period ; Repeat! 
 end
